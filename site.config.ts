3a8b05571cfb3f719bfbe099808ea8067ee8317b
Bigzy9527 <121087168+Bigzy9527@users.noreply.github.com>
2022-12-20 15:59:41 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,17 +9,17 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Jane Blog',
+  domain: 'https://bigzy9527.notion.site/14aad4ea247a4407a52148bf5ebcc425',
+  author: 'Jane Fang',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
+  twitter: '',
   github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  linkedin: 'zyfang',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
